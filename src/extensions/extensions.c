3fc0817b94ea02d1347a20614e085c8299ef30e4
Alex Baranowski <alex@euro-linux.com>
2020-01-19 15:46:01 +0100

    els-ASAP auto commit file/tag telepathy-logger-0.8.0-2.el7.src.rpm

---
+++ src/extensions/extensions.c
@@ -0,0 +1,9 @@
+#include "config.h"
+
+#include "extensions.h"
+
+#include <telepathy-glib/proxy-subclass.h>
+
+/* auto-generated stubs */
+#include "_gen/gtypes-body.h"
+#include "_gen/interfaces-body.h"
